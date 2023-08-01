3c85393f5c6fc455631cf70e424ba0acdf30af1c
Peter Ng <ngy@vaniercollege.qc.ca>
2023-08-01 16:08:20 -0400

    basic setup

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 const { createApp, ref } = Vue;
 
-createApp({
+let app = createApp({
   setup() {
-    const message = ref("Hello vue!");
+    const message = ref("Hello ");
     return {
       message,
     };
+  },
+  data() {
+    return {
+      name: "Peter",
+      imageSrc: "https://placehold.co/300x200",
+      availability: true,
+      av: "av-label",
+      nav: "av-label disabled",
+      age: 25,
+      students: [
+        { id: 1, name: "Patrick", age: 26 },
+        { id: 2, name: "John", age: 31 },
+        { id: 3, name: "Mary", age: 32 },
+      ],
+    };
+  },
+  methods: {
+    action() {
+      console.log("Action");
+    },
   },
 }).mount("#app");
