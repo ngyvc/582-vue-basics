0b535d29b9fa7b7fa04ba3fd35f803ba76448a2f
Peter Ng <ngy@vaniercollege.qc.ca>
2023-08-02 11:11:37 -0400

    add new sample component

---
+++ js/components/TeacherProfile.js
@@ -0,0 +1,32 @@
+app.component("teacher-profile", {
+  template: /*html*/ `
+    <div>
+        <h2>{{name}}</h2>
+        <p>{{age}} - {{availability}}</p>
+
+        <img v-if="profile" :src="profile" :alt="name" />
+        <img v-else src="https://placehold.co/100x100" :alt="name" />
+
+        <p  v-if="availability">Available</p>
+        <p v-else>Not Available</p>
+    </div>
+    `,
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    profile: {
+      type: String,
+      required: false,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    availability: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
